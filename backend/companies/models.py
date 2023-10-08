c583dbb7b1fd74fd44c23ba15b7915cd157b26a5
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-10-08 13:20:31 +0300

    Models
    Youtube Efe Görkem Ümit

--- backend/companies/models.py
+++ backend/companies/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class CompanyUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email alanı zorunlu.')
+        email= self.normalize_email(email)
+        user= self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using = self.db)
+        return user
+
+class CompanyUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    #logo
+    description = models.TextField(null=True, blank=True)
+    membership_start_date = models.DateField()
+    membership_end_date = models.DateField()
+    product_count= models.PositiveIntegerField()
+    address = models.TextField()
+    phone_number = models.CharField(max_length=15)
+    company_name = models.CharField(max_length=255)
+    is_staff= models.BooleanField(default=False)
+
+    objects = CompanyUserManager()
+
+    USERNAME_FIELD ='email'
+
+    def __str__(self):
+        return self.email
+
--- backend/customers/models.py
+++ backend/customers/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class CustomerUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email alanı zorunlu.')
+        email= self.normalize_email(email)
+        user= self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using = self.db)
+        return user
+
+class CustomerUserProfile(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=15)
+    company_name = models.CharField(max_length=255)
+    is_staff= models.BooleanField(default=False)
+
+    objects = CustomerUserManager()
+
+    USERNAME_FIELD ='email'
+
+    def __str__(self):
+        return self.email
+
