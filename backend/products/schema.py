ca0e10dafa354cb67d1b53876ad6a063561e075e
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-10-09 11:54:52 +0300

    Category Document 2
    Youtube Efe Görkem Ümit

---
+++ backend/products/schema.py
@@ -0,0 +1,17 @@
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
+from drf_spectacular.types import OpenApiTypes
+
+from .serializer import CategorySerializer
+
+category_doc = extend_schema(
+    responses = CategorySerializer(many=True),
+     parameters=[
+            OpenApiParameter(
+                name='slug', 
+                description='Category for slug ', 
+                type=OpenApiTypes.STR,
+                location = OpenApiParameter.QUERY
+                ),
+           
+        ],
+)
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,10 +1,48 @@
 from django.shortcuts import render
-from rest_framework import routers, serializers, viewsets
+from rest_framework import viewsets
 from .models import Category
 from .serializer import CategorySerializer
+from rest_framework.response import Response
+from .schema import category_doc
 
-
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(viewsets.ViewSet):
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer
 
+    @category_doc
+    def list(self, request):
+        """
+        Retrieve a list of all categories.
+
+        This endpoint returns a serialized list of all categories available in the system.
+
+        Parameters:
+        - `request`: The HTTP request object.
+
+        Returns:
+        A JSON response containing a list of serialized categories.
+
+        Example:
+        {
+            "categories": [
+                {
+                    "id": 1,
+                    "title": "Category 1",
+                    "slug": "category-1",
+                    "photo": "category/1/server/photo.jpg"
+                },
+                {
+                    "id": 2,
+                    "title": "Category 2",
+                    "slug": "category-2",
+                    "photo": null
+                }
+            ]
+        }
+        """
+
+        slug = request.query_params.get("slug")
+        if slug:
+            self.queryset = self.queryset.filter(slug = slug)
+
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
