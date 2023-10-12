fa4705ae251cf706a612d7b164882e88a16b9e7b
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-10-12 12:22:35 +0300

    Filter Shop
    Youtube Efe Görkem Ümit

--- vite-project/src/pages/Shop.tsx
+++ vite-project/src/pages/Shop.tsx
@@ -3,18 +3,20 @@ import React, {useEffect, useState} from "react";
 import {MEDIA_URL, BASE_URL} from '../configUrl'
 import Product from "../components/Product";
 
-function Shop() {
+const Shop =() => {
+    const [products, setProducts] =useState([])
+    const [filters, setFilters] = useState({
+        category:'',
+        min_price:'',
+        max_price:'',
+        qty:'',
+        size:'',
+        title:'',
+        brandid:'',
+    });
 
-    const[products, setProducts] =useState([])
 
-    useEffect(()=>{
-      axios.get(BASE_URL+'/product/select/').then((response)=>{
-        
-        setProducts(response.data);
-
-      })
-    }, [] )
-
+   
     const[categories, setCategories] =useState([])
 
     useEffect(()=>{
@@ -36,6 +38,40 @@ function Shop() {
         })
       }, [] )
 
+      useEffect(()=>{
+        fetchProducts();
+      }, [filters])
+
+      const fetchProducts = () =>{
+        const queryParams = new URLSearchParams(filters);
+
+        axios.get(BASE_URL+`/product/select/?${queryParams.toString()}`).then((response)=>{
+            setProducts(response.data);
+        })
+        .catch((error)=>{
+            console.error('Error : ', error);
+        })
+
+      }
+
+      const handleChange = (e) =>{
+        const {name, value} = e.target;
+        setFilters((prevFilters)=>({
+            ...prevFilters, [name]:value,
+        }))
+
+      }
+      const clearFilters = () => {
+        setFilters({
+          category: "",
+          min_price: "",
+          max_price: "",
+          qty: "",
+          size: "",
+          title: "",
+          brandid: "",
+        });
+      };
 
   return (
       
@@ -74,6 +110,8 @@ function Shop() {
                         <input 
                         type="radio" 
                         name="category"
+                        value={category.id}
+                        onChange={handleChange}
                         className="text-primary focus:ring-0 cursor-pointer rounded-sm"/>
                         <label className="ml-3 text-gray-950">{category.title}</label>
                         <div className="ml-auto text-gray-600 text-sm">({category.product_count})</div>
@@ -103,7 +141,9 @@ function Shop() {
                     <div key={brand.id}  className="flex items-center mt-1">
                         <input 
                         type="radio" 
-                        name="category"
+                        name="brandid"
+                        value={brand.id}
+                        onChange={handleChange}
                         className="text-primary focus:ring-0 cursor-pointer rounded-sm"/>
                         <label className="ml-3 text-gray-950">{brand.title}</label>
                         <div className="ml-auto text-gray-600 text-sm">({brand.product_count})</div>
@@ -127,9 +167,9 @@ function Shop() {
                 <h3 className="text-xl text-gray-800 uppercase mb-4 mt-3 font-medium">Price</h3>
 
                 <div className="mt-4 flex items-center">
-                    <input className="w-full border-gray-500 py-1 px-3 text-gray-600 text-sm shadow-sm rounded" placeholder="min" />
+                    <input className="w-full border-gray-500 py-1 px-3 text-gray-600 text-sm shadow-sm rounded" type="text"   name="min_price"  onChange={handleChange} placeholder="min" />
                    <span className="mx-3 text-gray-600"> - </span>
-                    <input className="w-full border-gray-500 py-1 px-3 text-gray-600 text-sm shadow-sm rounded" placeholder="min" />
+                    <input className="w-full border-gray-500 py-1 px-3 text-gray-600 text-sm shadow-sm rounded" type="text"  name="max_price"  onChange={handleChange} placeholder="max" />
 
 
                 </div>
@@ -144,7 +184,7 @@ function Shop() {
                
                 <div className="size-selector">
 
-                    <input type="radio" name="size" className="hidden" id="size-xs"/>
+                    <input type="radio" name="size" value="xs"  onChange={handleChange} className="hidden" id="size-xs"/>
                     <label for="size-xs" className="text-xs border border-gray-200  rounded-sm h-7 w-7 flex 
                     items-center justify-center cursor-pointer shadow-sm text-gray-900" >XS</label>
 
@@ -152,7 +192,7 @@ function Shop() {
 
                 <div className="size-selector">
 
-                    <input type="radio" name="size" className="hidden" id="size-s"/>
+                    <input type="radio" name="size" value="s"  onChange={handleChange} className="hidden" id="size-s"/>
                     <label for="size-s" className="text-xs border border-gray-200  rounded-sm h-7 w-7 flex 
                     items-center justify-center cursor-pointer shadow-sm text-gray-900" >S</label>
 
@@ -160,7 +200,7 @@ function Shop() {
 
                 <div className="size-selector">
 
-                    <input type="radio" name="size" className="hidden" id="size-m"/>
+                    <input type="radio" name="size" value="m"  onChange={handleChange}  className="hidden" id="size-m"/>
                     <label for="size-m" className="text-xs border border-gray-200  rounded-sm h-7 w-7 flex 
                     items-center justify-center cursor-pointer shadow-sm text-gray-900" >M</label>
 
@@ -168,7 +208,7 @@ function Shop() {
 
                 <div className="size-selector">
 
-                    <input type="radio" name="size" className="hidden" id="size-l"/>
+                    <input type="radio" name="size" value="l"  onChange={handleChange} className="hidden" id="size-l"/>
                     <label for="size-l" className="text-xs border border-gray-200  rounded-sm h-7 w-7 flex 
                     items-center justify-center cursor-pointer shadow-sm text-gray-900" >L</label>
 
@@ -177,7 +217,7 @@ function Shop() {
 
                 <div className="size-selector">
 
-                    <input type="radio" name="size" className="hidden" id="size-xl"/>
+                    <input type="radio" name="size" value="xl"  onChange={handleChange} className="hidden" id="size-xl"/>
                     <label for="size-xl" className="text-xs border border-gray-200  rounded-sm h-7 w-7 flex 
                     items-center justify-center cursor-pointer shadow-sm text-gray-900" >XL</label>
 
