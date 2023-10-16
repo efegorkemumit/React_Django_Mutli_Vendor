69e2dfcc9ec33d7047311d97fbf6b4dc9069ba8d
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-10-16 14:25:22 +0300

    userprofile update
    Youtube Efe Görkem Ümit

--- backend/customers/urls.py
+++ backend/customers/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import CustomersUserRegisterView, MyTokenObtainPairView
+from .views import CustomersUserRegisterView, MyTokenObtainPairView, UserProfileView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -18,6 +18,7 @@ urlpatterns = [
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
 
   
 ]
--- backend/customers/views.py
+++ backend/customers/views.py
@@ -3,12 +3,13 @@ from rest_framework.views import APIView
 from django.contrib.auth.hashers import make_password
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import CustomerUserRegisterSerializer, MyTokenObtainPairSerializer
+from .serializers import CustomerUserRegisterSerializer, MyTokenObtainPairSerializer, UserSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import CustomerUserProfile
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class= MyTokenObtainPairSerializer
@@ -25,3 +26,19 @@ class CustomersUserRegisterView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user_profile = CustomerUserProfile.objects.get(id=request.user.id)
+        serializer = UserSerializer(user_profile)
+        return Response(serializer.data)
+    def put(self, request):
+        user_profile = CustomerUserProfile.objects.get(id=request.user.id)
+        serializer = UserSerializer(user_profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message":"Profile Update"})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
